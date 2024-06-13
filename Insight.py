4e5d3c0dad76143f0122a617d847393c947d274b
Anantadhirya <anantadhirya@gmail.com>
2024-06-13 09:26:04 +0700

    feat: multiple simulations and final insights

--- Insight.py
+++ Insight.py
@@ -1,6 +1,8 @@
 from Settings import *
 import Utils
 
+import pandas as pd
+
 class Insight:
     def __init__(self):
         # 1 = lift
@@ -26,3 +28,26 @@ class Insight:
         print(f"Percentage (stairs : lift) = {self.percentage_choice(0):.2f} : {self.percentage_choice(1):.2f}")
         print(f"Average time by floor (stairs) =", [Utils.time_string(self.person_total_time[0][floor] / self.person_count[0][floor]) if self.person_count[0][floor] else "-" for floor in range(floor_count)])
         print(f"Average time by floor (lift) =", [Utils.time_string(self.person_total_time[1][floor] / self.person_count[1][floor]) if self.person_count[1][floor] else "-" for floor in range(floor_count)])
+    @staticmethod
+    def insights_to_csv(insights):
+        result1_df = pd.DataFrame([{"avg_stairs": insight.person_time_average(0), "avg_lift": insight.person_time_average(1)} for insight in insights])
+        result1_df.to_csv('insights1.csv', index=False)
+
+        result_total = [[0 for _ in range(floor_count)] for _ in range(2)]
+        result_count = [[0 for _ in range(floor_count)] for _ in range(2)]
+        for insight in insights:
+            for i in range(2):
+                for floor in range(floor_count):
+                    result_total[i][floor] += insight.person_total_time[i][floor]
+                    result_count[i][floor] += insight.person_count[i][floor]
+        result_avg = [[result_total[i][floor] / result_count[i][floor] if result_count[i][floor] else None for floor in range(floor_count)] for i in range(2)]
+        result2_df = pd.DataFrame([{"avg_stairs": result_avg[0][floor], "avg_lift": result_avg[1][floor]} for floor in range(floor_count)])
+        result2_df.to_csv('insights2.csv', index=False)
+
+        result_wait_time = []
+        for insight in insights:
+            for wait_time in insight.list_lift_wait_time:
+                result_wait_time.append(wait_time)
+        result3_df = pd.DataFrame([{"wait_time": wait_time} for wait_time in result_wait_time])
+        result3_df.to_csv('insights3.csv', index=False)
+        
--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from Model import Model
+from Insight import Insight
 
+# To run multiple simulations
+# simulation_count = 20
+# insights = []
+# for i in range(simulation_count):
+#     print(f"Running simulation {i}")
+#     model = Model(600, [-0.097, 14.029], 8, [83.70, 23.89], [-17.083, 7.683], [5, 2], False, True)
+#     model.run_simulation()
+#     insights.append(model.insight)
+# Insight.insights_to_csv(insights)
+
+# To run a single simulation with display
 model = Model(600, [-0.097, 14.029], 8, [83.70, 23.89], [-17.083, 7.683], [5, 2], True)
 model.run_simulation()
